99153aa377688cc2dfc7c6a04550642eeb1ee76a
Igor Obradovic <igor.obrado@gmail.com>
2022-12-27 17:27:58 +0100

    Project structure

---
+++ code/source/DirectXPlayground.cpp
@@ -0,0 +1,10 @@
+// DirectXPlayground.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "include/framework.h"
+
+// TODO: This is an example of a library function
+void fnDirectXPlayground()
+{
+}
